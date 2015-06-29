2a612d0b03d53eec64065c5f5348e7dae3c6753a
Alex <aleksandernasr@gmail.com>
2015-06-29 19:51:11 +0100

    Trying to get page_content to have Makers Academy

--- app/bookmark_manager.rb
+++ app/bookmark_manager.rb
@@ -1,14 +1,14 @@
 require 'sinatra/base'
-require_relative './models/link'
+require './app/models/link'
 
 class BookmarkManager < Sinatra::Base
+
+  set :views, proc {File.join(root,'.', 'views')}
+
 
   get '/links' do
     @links = Link.all
     erb :'links/index'
   end
 
-  run! if app_file == $0
 end
-
-

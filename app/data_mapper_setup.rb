1e001a570e3295ade3b12b458388b2e0c6a40942
Alex <aleksandernasr@gmail.com>
2015-06-30 10:52:54 +0100

    set up bookmark_manager_test database and RACK_ENV

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,7 +1,9 @@
+env = ENV['RACK_ENV'] || 'development'
+
 require 'data_mapper'
 require 'dm-postgres-adapter'
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager")
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require_relative './models/link'
 

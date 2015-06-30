9092cbba162e26f9ee776fd610500a417c73c612
Makers Laptop 88 <makerslaptop88@Makerss-MacBook-Pro.local>
2015-06-30 11:03:17 +0100

    new test

--- spec/features/listing_all_links_spec.rb
+++ spec/features/listing_all_links_spec.rb
@@ -8,22 +8,5 @@ feature 'User browses the list of links' do
       expect(page).to have_content('Makers Academy')
     end
   end
+
 end
-
-
-
-# feature 'Creating links' do
-
-#   scenario 'I can create a new link' do
-#     visit '/links/new'
-#     fill_in 'url',   with: 'http://www.zombo.com/'
-#     fill_in 'title', with: 'This is Zombocom'
-#     click_button 'Create link'
-
-#     expect(current_path).to eq '/links'
-
-#     within 'ul#links' do
-#       expect(page).to have_content('This is Zombocom')
-#     end
-#   end
-# end

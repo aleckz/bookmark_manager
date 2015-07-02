1aec4e9a89608a44fc13e97b8b66660b52d0e64c
Simon Legg <leggsimon@gmail.com>
2015-07-02 11:08:13 +0100

    Modified bookmarks_web.rb

--- app/bookmarks_web.rb
+++ app/bookmarks_web.rb
@@ -50,7 +50,7 @@ class BookmarksWeb < Sinatra::Base
 
   helpers do
     def current_user
-      @current_user ||= User.get(session[:user_id]) if session[:user_id]
+      @current_user ||= User.get(session[:user_id])
     end
   end
 
